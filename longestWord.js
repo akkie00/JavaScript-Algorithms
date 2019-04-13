bac919a8680edcba1f7bab98332a1c931399649c
akkie00 <akshay8801.ak@gmail.com>
2019-04-13 11:57:40 +0530

    added longestWord algo

--- longestWord.js
+++ longestWord.js
@@ -1,4 +1,19 @@
 function longestWord(str){
 
+    let strArr = str.replace(/[\W]/g, ' ').split(' ');
+    console.log(strArr)
+
+    let longest = '';
+
+    for (let i = 0; i < strArr.length; i++){
+        // console.log(strArr[i].length)
+        if(strArr[i].length > longest.length){
+            longest = strArr[i];
+        }
+    }
+    return longest;
 }
 
+console.log(longestWord('Ready, steady, go!'));
+console.log(longestWord('Ready[][], steadyyy, GOOO!!!'));
+console.log(longestWord('give me an , dfalkfj, kasldkfja'));
